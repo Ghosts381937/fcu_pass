de4324f21d582c0823b078b0d2e49d9ade5673aa
fcu-d0813127 <fcu-d0813127>
2022-01-14 17:50:41 +0800

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,81 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+class FcuPass extends React.Component {
+  // dateConvert = () => {
+
+  // }
+
+  // getDate = () => {
+
+  // }
+
+  render() {
+    return (
+      <div className="body_item">
+        <div className="circle backgcolor">
+          <div className="content">
+            <div className="date">01/14</div>
+            <div className="dvtitle">{this.props.name}</div>
+            <div className="ng-scope">
+              <div className="dvtitle">自主健康管理</div>
+              <div className="pass">PASS</div>
+            </div>
+            <div className="footer">逢甲大學關心您</div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+class ChangeNameButton extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      name: ""
+    };
+    this.getAllCacheData();
+  }
+
+  getAllCacheData = async () => {
+    const url = "http://localhost:3000/";
+    const names = await caches.keys();
+    for (var i = 0; i < names.length; i++) {
+      const cacheStorage = await caches.open(names[i]);
+      const cacheResponse = await cacheStorage.match(url);
+      const data = await cacheResponse.json();
+      this.setState({name: data});
+    }
+  }
+
+  addDataIntoCache = (cacheName, url, response) => {
+    const data = new Response(JSON.stringify(response));
+    if ("caches" in window) {
+      caches.open(cacheName).then((cache) => {
+        cache.put(url, data);
+      });
+    }
+  }
+
+  changeHandler = () => {
+    const name = prompt("Please enter your name: ");
+    this.setState({name: name});
+    this.addDataIntoCache("test", "http://localhost:3000/", name);
+  }
+
+  render() {
+    return (
+      <div className="outside_button">
+        <FcuPass name={this.state.name}/>
+        <button className="button" onClick={this.changeHandler}>Change Name</button>
+      </div>
+    )
+  }
+}
+
+ReactDOM.render (
+  <ChangeNameButton />, 
+  document.getElementById("root")
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
